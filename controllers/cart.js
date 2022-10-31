5e46675d275b75d87b18120c5316299482a0e117
manuel sierra <manuelsierra@MacBook-Pro-de-manuel.local>
2022-10-31 17:23:43 -0300

    cart controller changes, not tested

--- controllers/cart.js
+++ controllers/cart.js
@@ -3,55 +3,84 @@ const User = require('../models/user');
 const Product = require('../models/product');
 const { response } = require('express');
 
-const getCarts = async(req, res=response)=>{
+const getCarts = async (req, res = response) => {
     const carts = await Cart.find({}).populate('user');
 
-    if(!carts){
+    if (!carts) {
         return res.status(404).json({
-            ok:false,
-            msg:'No carts were founded'
+            ok: false,
+            msg: 'No carts were founded'
         })
     }
 
     res.status(200).json({
-        ok:true,
+        ok: true,
         carts
     })
-    
+
 }
 
-const getCartbyId = async(req, res)=>{
+const getCartbyId = async (req, res) => {
     const cartOwner = req.params.uid
 
-    const cart = await User.findById({cartOwner});
+    const cart = await User.findById({ cartOwner });
 
-    if(!cart){
+    if (!cart) {
         return res.status(404).json({
-            ok:false,
+            ok: false,
             msg: 'Cart not found.'
         })
     }
 
     res.status(200).json({
-        ok:true,
+        ok: true,
         cart
     });
 }
 
 
-const postCart = async (req, res)=>{
-    const user = req.user.uid;
-    const cart = req.params.id;
-    const userDB = await User.findById({user});
-    const cartDB = await Cart.findById({cart});
+const postCart = async (req, res) => {
+    const owner = req.user.uid;
+    const { productID, quantity } = req.body;
 
-    
+    try {
+        const cart = Cart.findOne({ owner });
+        const product = Product.findOne({ productID });
+
+        if (!product) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Product not found'
+            })
+        }
+
+        const price = product.salePrice;
+        const name = product.name;
+
+        if (cart) {
+            const prodIndex = cart.products.findIndex(prod => prod._id === productID)
+
+
+            if (prodIndex > -1) {
+                let cartProduct = cart.products[prodIndex];
+            }
+        }
+
+
+
+    } catch (error) {
+
+    }
+
+
+
 }
 
 
-module.exports={
+module.exports = {
     getCarts,
-    getCartbyId
+    getCartbyId,
+    postCart
 }
 
 
